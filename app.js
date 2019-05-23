bc558165bab6375d1a133b34d58c52795d63416a
shijue1 <shengqiputixin@aliyun.com>
2019-05-23 11:34:39 +0800

    。

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
-const app = require("express")()
-const route = require('./config/route')
-
+const app = require("express")();
+const route = require("./config/route");
+const express = require("express");
 //设置允许跨域访问该服务.
-app.all('*', (req, res, next) => {
+app.all("*", (req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Content-Type,Content-Length, Authorization, Accept,X-Requested-With"
+  );
   res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
   res.header("Access-Control-Allow-Credentials", true);
-    next();
-  });
+  next();
+});
 
-require('./util/register-route')(route, app)
+app.use(express.static("assets"));
 
-app.listen(require('./config/app').PORT)
+require("./util/register-route")(route, app);
+
+app.listen(require("./config/app").PORT);
